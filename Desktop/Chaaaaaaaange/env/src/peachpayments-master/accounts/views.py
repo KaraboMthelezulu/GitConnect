d8a5070fb2ce153a560c14560a676802b52fe55c
KaraboMthelezulu <180139@virtualwindow.co.za>
2019-07-17 09:59:51 +0200

    msg 3

---
+++ Desktop/Chaaaaaaaange/env/src/peachpayments-master/accounts/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render, redirect
+from rest_framework import generics
+
+from . import models
+from . import serializers
+from .models import User
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from peachpayments.tokens import account_activation_token
+from django.core.mail import EmailMessage
+
+from django.contrib.auth import authenticate, login
+
+class UserListView(generics.ListCreateAPIView):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+def register(request):
+    form = CustomUserCreationForm
+    success_url = '/dashboard/packages'
+    template_name = 'landing/pages/registration_page.html'
+
+    username_validation = ''
+    email_validation    = ''
+    password_validation = ''
+    pass_c = ''
+    email_c= ''
+
+    if request.method == 'POST':
+
+
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+
+            if(User.objects.filter(username = request.POST['email']).exists()):
+                email_validation = "email already exists"
+                email_c= 'red'
+                
+            elif(request.POST['password2'] != request.POST['password1']):
+                password_validation = "passwords do not match"
+                pass_c = 'red'
+            
+            else:
+                import os
+                import base64
+
+
+                user = form.save(commit=False)
+                user.is_active = True
+                user.username = request.POST['email']
+                user.first_name = request.POST['first_name']
+                user.last_name = request.POST['last_name']
+                user.phone = request.POST['phone']
+                user.user_id = base64.b16encode(os.urandom(6)).decode('ascii')
+                user.save()
+                    
+                
+
+                username = request.POST.get('email', '')
+                password = request.POST.get('password1', '')
+
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    sendEmail(request)
+                    return redirect('/dashboard/packages')
+            
+
+        return render(request, template_name, 
+                            {'username_validation':username_validation,
+                                'email_validation':email_validation,
+                                'password_validation':password_validation,
+                                 'pass_c':pass_c,'emai_c':email_c})
+
+        
+    else:
+        #form = SignupForm()
+        form_class = CustomUserCreationForm
+    return render(request, template_name, {})
+    
+def sendEmail(request):
+    mail_subject = 'Welcome.'
+    message = render_to_string('landing/pages/auth/acc_active_email.html', {})
+    to_email = request.user.email
+    email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+    email.send()
+
+    
+
+
---
+++ Desktop/Chaaaaaaaange/env/src/peachpayments-master/landing/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from accounts.models import User
+from django.contrib.auth import authenticate, login
+# Create your views here.
+def landing_page(request):
+
+
+    return render(request, 'new-landing/home-new.html', {})
+
+def login_in(request):
+    if request.method == 'GET':
+        context = ''
+        return render(request, 'mytest/login.html', {'context': context})
+
+    elif request.method == 'POST':
+        username = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/dashboard/packages')
+            # return HttpResponseRedirect('/')
+        else:
+            context = {'error': 'Wrong credintials'}  # to display error?
+            return render(request, '/registration', {'context': context})
+
+def reg_page(request):
+
+    pass_c = ''
+    email_c= ''
+    password_validation= 'Confirm Password'
+    email_validation  = 'Email'
+    if request.POST:
+        user_create = User()
+        user_create.username = request.POST['email']
+        user_create.email = request.POST['email']
+        user_create.first_name = request.POST['first_name']
+        user_create.last_name = request.POST['last_name']
+        user_create.rep = request.POST['rep_id']
+        user_create.phone = request.POST['phone']
+        user_create.save()
+        return redirect('dashboard/packages')
+    else:
+        pass
+    return render(request, 'landing/pages/registration_page.html', {'password_validation':password_validation , 'email_validation':email_validation, 'pass_c':pass_c,'emai_c':email_c})
+ 
+
+def referral(request):
+
+    
+    return render(request, 'landing/pages/referral.html', {})
+
+
+
+def newhome(request):
+    return render(request, 'landing/pages/home.html',{})
